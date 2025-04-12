f074b588c77e59be26d3467a4c44fb174b3c594d
jalalbmnf <calalmnf@gmail.com>
2025-04-13 01:34:05 +0400

    💄 Added API error handling, loading state, fixed bugs and etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import Copy from "./assets/icons/copy.svg";
+import Check from "./assets/icons/check.svg";
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [link, setLink] = useState("");
-  const [shortenedLink, setShortenedLink] = useState("")
+  const [shortenedLink, setShortenedLink] = useState("");
+  const [copied, setCopied] = useState(false);
 
   const createShortLink = () => {
-    setLoading(true)
+    setLoading(true);
     axios
       .post(
         `https://api.tinyurl.com/create?api_token=y1SRRICkItO5MKkzWEECOUe4HBc6otoIi4CH3NcomUK3AZCj9tOVKQn3T2Bd`,
@@ -22,19 +26,44 @@ function App() {
         }
       )
       .then((result) => {
-        const { data: { data: { tiny_url } } } = result || {};
+        const {
+          data: {
+            data: { tiny_url },
+          },
+        } = result || {};
         if (tiny_url) {
-          setShortenedLink(tiny_url)
+          setShortenedLink(tiny_url);
         }
       })
-      .catch((err) => setError(err?.message));
+      .catch((err) => setError(err?.message))
+      .finally(() => {
+        setLink("");
+        setLoading(false);
+      });
   };
 
   const copyToClipboard = () => {
     if (link) {
-      window.navigator.clipboard.writeText(shortenedLink)
+      window.navigator.clipboard.writeText(shortenedLink).then(() => {
+        setCopied(true);
+      });
     }
   };
+
+  useEffect(() => {
+    const expireCopyTime = setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+
+    return () => clearTimeout(expireCopyTime);
+  }, [copied]);
+
+  if (loading)
+    return (
+      <div className="w-full h-screen flex justify-center items-center text-4xl text-slate-800 font-extrabold">
+        Loading...
+      </div>
+    );
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center bg-transparent">
@@ -56,7 +85,30 @@ function App() {
           Short link
         </button>
       </div>
-      <div>{shortenedLink && window.navigator.clipboard && <button className="bg-red-400" onClick={copyToClipboard}>Copy to clipboard</button>}</div>
+      <div>
+        {shortenedLink && window.navigator.clipboard && (
+          <div className="mt-3 flex justify-center items-center gap-2">
+            <p className="text-base border border-slate-800 p-2 rounded-md">
+              {shortenedLink}
+            </p>
+            <button
+              className="rounded-md bg-transparent p-2 cursor-pointer border border-slate-800 text-center text-sm text-slate-800 transition-all shadow-md hover:shadow-lg focus:bg-transparent focus:shadow-none active:bg-transparent hover:bg-transparent active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              onClick={copyToClipboard}
+            >
+              {copied ? (
+                <img src={Check} alt="Check icon" />
+              ) : (
+                <img src={Copy} alt="Copy icon" />
+              )}
+            </button>
+          </div>
+        )}
+        {error && (
+          <div className="text-lg text-red-500 font-bold">
+            {error || "Something went wrong"}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
